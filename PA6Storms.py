10b36bd6a80465c0e1db839b2f4426624ae249d2
Holly <hfwright@loyola.edu>
2016-12-07 18:07:55 -0500

    fixed main

--- PA6Storms.py
+++ PA6Storms.py
@@ -12,9 +12,7 @@
 # Purpose: Ask the user for a filename
 # Parameters: none
 # Return: filename
-
 import os
-
 
 def inputFile():
     filename = input("Please input a file to read from.")
@@ -25,7 +23,7 @@ def inputFile():
 
 
 # Purpose: read from the file
-# Paramaters:filename
+# Parameters: filename
 # Return:list
 
 def read_file(filename):
@@ -46,7 +44,7 @@ def read_file(filename):
 # Parameters:storm list
 # Return:graph
 
-def storm_graph_dict(storms_list1): # Shoud be dictionary******
+def storm_graph_dict(storms_list1):
     import pylab
     graph_dict = {}
     for graph_dict in storms_list1:
@@ -78,7 +76,7 @@ def storm_graph(graph_dict):
 
 
 def storm_years(storm_list1):
-    storm_dict={}
+    storm_dict = {}
     for line in storm_list1:
         if line[7] not in storm_dict:
             storm_dict[line[7]] = 1
@@ -89,8 +87,8 @@ def storm_years(storm_list1):
 
 
 def storm_years_secondfile(given_storm):
-    file2=input("Please enter another file you would like to input and compare to the original file")
-    new_file=read_file(file2)
+    file2 = input("Please enter another file you would like to input and compare to the original file")
+    new_file = read_file(file2)
     storm_years(new_file)
 
 
@@ -110,7 +108,7 @@ def comparison_of_years(outfile_name,dict_1,dict_2):
 
 
 # Function Name:common_storm
-# Purpose:What is the most common storm for a particular state?  **Line is not the variable - find out what you're looking for
+# Purpose:What is the most common storm for a particular state?
 # Parameters:storm_list, given_state **** If everything else works, make print(line) and see what comes out
 # Return:storm_dict
 def common_storm(storm_list1,given_state):
@@ -135,7 +133,7 @@ def common_storm_max(storm_dict):
     return biggest_value
 
 
-# Function Name:injuries_state
+# Function Name:injuries_in_a_state
 # Purpose:How many injuries in a specific state
 # Parameters:storm_list, given_state2
 # Return:how many injuries
@@ -148,14 +146,29 @@ def injuries_in_a_state(given_state2, storm_list1):
 
 
 def menu():
-    print("The choices you have to choose from to learn more about storms are most  common storms that happened in a certain year,"+
-          " how many injuries from storms in a specific state,most common storms that happened in a particular state ")
+    print("The choices you have to choose from to learn more about storms are: (A) A graph showing the number of storms in a certain month in a particular state,(B) most common storms that happened in a certain year,"+
+          "(C) how many injuries from storms in a specific state, (D) most common storms that happened in a particular state ")
     choice = input("What would you like to know about storms?")
     return choice
 
 def main():
     filename = inputFile()
     new_file = read_file(filename)
-    print(common_storm_max(new_file))
+    if choice == "A":
+        graph_dict =storm_graph_dict(new_file)
+        storm_graph(graph_dict)
+    if choice == "B":
+        given_storm = input("Please enter a storm type in which you would like to find the year it occurred.")
+        storm_years_secondfile(given_storm)
+        outfile_name = input("Please enter the name of an output file.")
+        comparison_of_years(outfile_name, dict_1, dict_2)
+    if choice == "C":
+        given_state2 = input("Please input a state.")
+        injuries_in_a_state(given_state2, storm_list1)
+    if choice == "D":
+        given_state = input("Please input a state.")
+        common_storm(storm_list1, given_state)
+        common_storm_max(storm_dict)
+    print("The storm with the highest number of occurances, occurred",common_storm_max(new_file),"times.")
 
 main()
